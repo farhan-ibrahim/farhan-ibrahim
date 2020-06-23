868e3499ac22d34c1ee128b4c90824d1ab51c4c1
farhan-ibrahim <farhan5543@gmail.com>
2020-06-23 17:39:14 +0800

    project page (mobile)

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// to show info on project
+
+// var projectCard = document.querySelector('projectCard');
+// var cardInner = document.querySelector('cardInner');
+
+// console.log(projectCard);
+// console.log(cardInner);
+
+// function showInfo() {
+//     console.log('i need info');
+//     cardInner.style.transform='rotateY(180deg)';
+// }
