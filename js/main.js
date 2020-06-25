7795022593694c85f8641ee1969d2f43a65df788
farhan-ibrahim <farhan5543@gmail.com>
2020-06-25 12:40:04 +0800

    mob project section animation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,60 @@
-// to show info on project
+// Project Section
+var infos = document.getElementsByClassName('modalDiv');
+console.log(infos);
 
-// var projectCard = document.querySelector('projectCard');
-// var cardInner = document.querySelector('cardInner');
+// function showInfo
+function showInfo(id){
+    // to show modal
+    document.getElementById('modal').style.display="flex";
+    //to show info
+    infos[id].style.display='inline';
+}
 
-// console.log(projectCard);
-// console.log(cardInner);
+// to hide info
+function hideModal(){
+    // to hide modal
+    document.getElementById('modal').style.display="none";
+    // to hide info
+    for (i=0; i < infos.length; i++){
+        infos[i].style.display='none';
+    }
+}
 
-// function showInfo() {
-//     console.log('i need info');
-//     cardInner.style.transform='rotateY(180deg)';
+
+
+
+
+// homepage animation
+// to move engineer up wheen in homepage
+
+var logo = document.getElementById('profileLogo')
+var home = document.getElementById('home');
+var textOne = document.getElementById('mainText1');
+var eng = document.getElementById('engineer');
+var prog = document.getElementById('programmer');
+
+// function homepageAnimation(){
+//     console.log("function is called");
+//     eng.style.animation ='engMoveLeft 2s linear 3s forwards';
+//     // textOne.style.animation ='textMoveDown 2s linear 3s forwards';
+//     prog.style.animation='progMoveLeft 2s linear 4s forwards';
 // }
+
+// // to call the function when home logo is clicked
+// logo.addEventListener('click', homepageAnimation());
+
+// var timer = setInterval(function(){
+//     console.log('interval running');
+
+//     rect = eng.getBoundingClientRect();
+//     x = rect.left;
+//     y = rect.top;
+//     console.log(y);
+//     if(y <= 220){
+//         console.log('change image');
+//         eng.src = 'images/programmer.svg';
+//         if (y == 33);{
+//             clearInterval(timer);
+//         }
+//     }        
+// }, 1000);
